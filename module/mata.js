e7f92a4d60b5c2698ccaba77a599f7e106c4a044
bustamante <ricardobqueiroz@gmail.com>
2016-08-08 04:37:01 -0300

    Mudando o modulo do comando "mata".

--- module/mata.js
+++ module/mata.js
@@ -1,16 +1,35 @@
 commandName = "/mata";
 
-frases = [
-	["Bateu em","lentamente."]
+acao = [
+"bateu em",
+"esmurrou",
+"espancou",
+"estuprou",
+"nocauteou",
+"matou",
+"deu infinitos socos em",
+"usou choque do trovão em",
+"molestou"
 ]
+
+efeito = [
+"",
+"com estilo",
+"lentamente",
+"sem piedade",
+"com o objetivo de matar",
+"só de maldade",
+"",
+"por vingança"
+]
+
 
 function Command(user, vs_msg) {
 	var msg = "";
 	if (!vs_msg) {
 		msg = user + ", diga alguém pra eu matar!";
 	} else {
-		msg = frases[0][0] + frases[0][1];
-		msg += "Será que deu?";
+		msg = user + " " + acao[Math.floor(Math.random()*acao.length)] + " " + vs_msg + " " + efeito[Math.floor(Math.random()*efeito.length)] + ".";
 	}
 	return msg;
 }
